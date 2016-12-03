f518fdb1a92ca44b25a639cd81dfdb02db4a0808
TylerKirkpatrick <tkirkpatrick@smu.edu>
2016-12-02 20:58:14 -0600

    connection to dB established

--- server/server.js
+++ server/server.js
@@ -1,7 +1,48 @@
 'use strict';
-
+var Joi = require('joi');
 const Hapi = require('hapi');
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123',
+  database : 'testDb'
+});
+//mysql connection
+var mysql = require('mysql');
+connection.connect();
+connection.query('SELECT * from users', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+connection.end();
+
+//end myql connection
+
+//quotes
+var quotes = [
+  {
+    author: 'Audrey Hepburn'
+  , text: 'Nothing is impossible, the word itself says \'I\'m possible\'!'
+  }
+, {
+    author: 'Walt Disney'
+  , text: 'You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you'
+  }
+, {
+    author: 'Unknown'
+  , text: 'Even the greatest was once a beginner. Don\'t be afraid to take that first step.'
+  }
+, {
+    author: 'Neale Donald Walsch'
+  , text: 'You are afraid to die, and you\'re afraid to live. What a way to exist.'
+  }
+];
+
+//START SERVER
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
@@ -9,15 +50,73 @@ server.connection({
     port: 8000 
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
+//hapi_mysql
 
-        return reply('hello world');
+//end hapi_mysql
+
+//
+//
+// Simulate an external module which is the correct way to expose this
+//    kind of functionality.
+//
+var quoteController = {};
+
+quoteController.getConfig = {
+  handler: function(req, reply) {
+    if (req.params.id) {
+      if (quotes.length <= req.params.id) return reply('No quote found.').code(404);
+      return reply(quotes[req.params.id]);
     }
-});
+    reply(quotes);
+  }
+};
+
+quoteController.getRandomConfig = {
+  handler: function(req, reply) {
+    var id = Math.floor(Math.random() * quotes.length);
+    reply(quotes[id]);
+  }
+};
+
+quoteController.postConfig = {
+  handler: function(req, reply) {
+    var newQuote = { author: req.payload.author, text: req.payload.text };
+    quotes.push(newQuote);
+    reply(newQuote);
+  },
+  validate: {
+    payload: {
+      author: Joi.string().required(),
+      text: Joi.string().required()
+    }
+  }
+};
+
+quoteController.deleteConfig = {
+  handler: function(req, reply) {
+    if (quotes.length <= req.params.id) return reply('No quote found.').code(404);
+    quotes.splice(req.params.id, 1);
+    reply(true);
+  }
+};
+//end quoteController
+
+
+//
+// ROUTES
+//
+var routes = [
+  { path: '/', method: 'GET', config: quoteController.getConfig },
+  { path: '/quote/{id?}', method: 'GET', config: quoteController.getConfig },
+  { path: '/random', method: 'GET', config: quoteController.getRandomConfig },
+  { path: '/quote', method: 'POST', config: quoteController.postConfig },
+  { path: '/quote/{id}', method: 'DELETE', config: quoteController.deleteConfig }
+];
+
+
+server.route(routes);
+
+//server.start();
 
 // Start the server
 server.start((err) => {
