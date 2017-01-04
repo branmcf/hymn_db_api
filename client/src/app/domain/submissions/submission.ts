ae4f16d09581ce7d694342ff49757811e6a3a38a
JustinSuh <jsuh@smu.edu>
2017-01-04 14:40:47 -0600

    working through the tough angular stuff now.

--- client/src/app/domain/submissions/submission.ts
+++ client/src/app/domain/submissions/submission.ts
@@ -1,5 +1,18 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
-export class Submission {
-
+export class SubmissionResources {
+    title: string;
+    type: string; 
+    url: string;
+    author: string;
+    parent: string;
+    desc: string; 
+    category: any[];
+    topic: any[];
+    accompany: any[];
+    lang: any[];
+    ensemble: any[];
+    ethnicity: any;
+    isInvolved: any;
+    free: any;
 }
