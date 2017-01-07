0cbd0d3348e4c4fbc9c8cf48d93c3ce91595f1c0
DNguyen17 <danhn@smu.edu>
2017-01-06 19:21:36 -0600

    updated packages and fixed navbar

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -3,7 +3,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
-import { EntryModule } from './entry/entry.module'
+import { EntryModule } from './entry/entry.module';
 
 @Component({
   selector: 'hymn-root',
