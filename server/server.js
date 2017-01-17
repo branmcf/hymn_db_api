f7c787e4c00607006c5591774a2d152a925fc6ee
TylerKirkpatrick <tkirkpatrick@smu.edu>
2017-01-16 23:02:57 -0600

    implemented hashing and storing salts and hashed passwords

--- server/server.js
+++ server/server.js
@@ -2,7 +2,150 @@
 var Joi = require('joi');
 const Hapi = require('hapi');
 var mysql = require('mysql');
+//Boom is an error library for internal error generation.
+const Boom = require('boom');
+const glob = require('glob');
+const path = require('path');
+const secret = require('./config');
+//bcrypt for salting/hashing passwords
+const bcrypt = require('bcrypt');
+var async = require('async');
 
+var hashDone = false;
+//
+// Create a server with a host and port
+//
+const server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+
+//
+//Bcrypt testing
+//
+var SALT_WORK_FACTOR = 10;
+//var pass = '123456789';
+
+function saltAndHash(pass) {
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if(err) { return console.error(err); }
+
+    bcrypt.hash(pass, salt, function(err, hash) {
+      if(err) { return console.error(err); }
+
+      console.log(hash);
+
+      //save new password
+      var newPass = hash;
+
+      bcrypt.compare(pass, hash, function(err, isMatch) {
+        if(err) { return console.error(err); }
+
+        console.log('do they match?', isMatch);
+
+        return newPass;
+      });
+
+    });
+  });
+}
+
+function hashAndStoreSync(user) {
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(user.password, salt);
+  
+  var returnedUser = {
+    password: hash,
+    salt: salt
+  }
+
+  return returnedUser;
+}
+
+function checkPassSync(user, thepass) {
+ 
+  console.log(bcrypt.compareSync(thehash, user.password));
+}
+
+function hashAndStoreAsync(user) {
+  //hash a pass
+  var plaintextPass = user.password;
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(plaintextPass, salt, function(err, hash) {
+      //store hash in password DB
+      console.log(hash, " of type: ", typeof hash);
+      if(!err) { 
+        console.log("no errors, change password to the hash...");
+        hashDone = true;
+        return hash; 
+      }
+    });
+  }); 
+}
+//
+// Another hash algorithm
+//
+function hashAndStoreAsync2(user) {
+
+  bcrypt.genSalt(10, function (err, salt) { 
+    console.log('salt: ' + salt); 
+    //
+    bcrypt.hash('test', salt, function (err, crypted) {
+      console.log('crypted: ' + crypted);
+
+      bcrypt.compare('test', crypted, function(err, res) {
+        console.log('compared true:' + res);
+      });
+    });
+
+  });
+}
+
+//
+//authentication
+//
+/*
+const Crypto = require('crypto');
+const onRequest = function (request, reply) {
+  const hash = Crypto.createHash('sha1');
+  request.on('peek', (chunk) => { hash.update(chunk); });
+  request.once('finish', () => { console.log(hash.digest('hex')); });
+  request.once('disconnect', () => { console.error('request aborted'); });
+  reply reply.continue();
+};
+
+server.ext('onRequest', onRequest);
+*/
+
+//
+// hashing test
+//
+server.register(require('hapi-auth-jwt'), (err) => {
+
+  // We're giving the strategy both a name
+  // and scheme of 'jwt'
+  server.auth.strategy('jwt', 'jwt', {
+    key: secret,
+    verifyOptions: { algorithms: ['HS256'] }
+  });
+
+  // Look through the routes in
+  // all the subdirectories of API
+  // and create a new route for each
+  glob.sync('api/**/routes/*.js', {
+    root: __dirname
+  }).forEach(file => {
+    const route = require(path.join(__dirname, file));
+    server.route(route);
+  });
+});
+
+//
+// MYSQL Connection
+//
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -47,14 +190,6 @@ var quotes = [
   }
 ];
 
-//START SERVER
-// Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
-
 //
 //
 // Simulate an external module which is the correct way to expose this
@@ -72,6 +207,7 @@ var congs = []
 - mysql -
 =================================================== 
 */
+
 connection.connect();
 
 //get users from db
@@ -80,7 +216,14 @@ connection.query('SELECT * from users', function(err, rows, fields) {
     //console.log('The solution is: ', rows);
     //console.log(rows[0].first_name, rows[0].last_name);
     //console.log(rows);
-    setValue(rows);
+    //console.log(JSON.stringify(rows));
+    //console.log(rows.rawDataPacket);
+
+    addToLocalArray(users, rows);
+
+    
+
+    //setValue(toReturn, users);
   }
   else
     console.log('Error while performing USers Query.');
@@ -93,7 +236,9 @@ connection.query('SELECT * from resources', function(err, rows, fields) {
     //console.log('The solution is: ', rows);
     //console.log(rows[0].first_name, rows[0].last_name);
     //console.log(rows);
-    setValue(rows, resources);
+    
+    addToLocalArray(resources, rows);
+
   }
   else
     console.log('Error while performing Resources Query.');
@@ -101,12 +246,12 @@ connection.query('SELECT * from resources', function(err, rows, fields) {
 });
 
 //get events from db
-connection.query('SELECT * from resources', function(err, rows, fields) {
+connection.query('SELECT * from events', function(err, rows, fields) {
   if (!err) {
     //console.log('The solution is: ', rows);
     //console.log(rows[0].first_name, rows[0].last_name);
     //console.log(rows);
-    setValue(rows, events);
+    addToLocalArray(events, rows);
   }
   else
     console.log('Error while performing Events Query.');
@@ -119,14 +264,12 @@ connection.query('SELECT * from congregations', function(err, rows, fields) {
     //console.log('The solution is: ', rows);
     //console.log(rows[0].first_name, rows[0].last_name);
     //console.log(rows);
-    setValue(rows, congs);
+    addToLocalArray(congs, rows);
   }
   else
     console.log('Error while performing Congregations Query.');
 
 });
-
-
 
 /* 
 ===================================================
@@ -134,14 +277,20 @@ connection.query('SELECT * from congregations', function(err, rows, fields) {
 =================================================== 
 */
 
-//function below for console logging queries above
-function setValue(value, whereToStore) {
-  whereToStore = value;
-  console.log(whereToStore);
+//function below for storing database content in local arrays
+function addToLocalArray(localArray, theRows) {
+
+  for (var i = 0; i < theRows.length; i++) {
+    var toReturn = JSON.stringify(theRows[i]);
+    localArray.push(toReturn);
+  }
+
+
 }
 
 var quoteController = {};
 var userController = {};
+var userController_2 = {};
 var resourceController ={};
 var eventController = {};
 var congController ={};
@@ -216,28 +365,45 @@ userController.postConfig = {
   handler: function(req, reply) {
 
     var newUser = { 
-      email: req.payload.email, 
-      password: req.payload.password, 
+      email: req.payload.email,  
+      password: req.payload.password,
       first_name: req.payload.first_name,
-      last_name: req.payload.last_name
+      last_name: req.payload.last_name,
+      salt: null
     };
+
+    var returnedUser = hashAndStoreSync(newUser);
+    if(returnedUser.password != undefined) {
+      newUser.password = returnedUser.password;
+      newUser.salt = returnedUser.salt;
+      console.log("hashed pass:", newUser.password, " with salt: ", newUser.salt);
+    }
+    else {
+      console.log("error with hash function, new password will default to \"password\". ");
+      newUser.password = "password";
+    }
+    
+    
 
 // mysql
     //connection.connect();
     connection.query(
       'INSERT INTO users SET ?', newUser,
       function(err, rows) {
+        if(err) {
+          throw new Error(err);
+          return;
+        }
+
         reply([{
           statusCode: 200,
           message: 'Inserted Successfully',
         }]);
-        if(err) {
-          throw new Error(err)
-        }
+        
       }
     );
     //DELETE BELOW TO MAKE CONTINUOUS INSERTS
-    connection.end();
+    //connection.end();
 
 //end mysql
 
@@ -290,13 +456,16 @@ resourceController.postConfig = {
     connection.query(
       'INSERT INTO resources SET ?', newRes,
       function(err, rows) {
+        if(err) {
+          throw new Error(err);
+          return;
+        }
+
         reply([{
           statusCode: 200,
           message: 'Inserted Successfully',
         }]);
-        if(err) {
-          throw new Error(err)
-        }
+        
       }
     );
     //end mysql
@@ -349,13 +518,16 @@ eventController.postConfig = {
     connection.query(
       'INSERT INTO event_table SET ?', newEvent,
       function(err, rows) {
+        if(err) {
+          throw new Error(err);
+          return;
+        }
+
         reply([{
           statusCode: 200,
           message: 'Inserted Successfully',
         }]);
-        if(err) {
-          throw new Error(err)
-        }
+        
       }
     );
     //end mysql
@@ -416,13 +588,16 @@ congController.postConfig = {
     connection.query(
       'INSERT INTO congregations SET ?', newCong,
       function(err, rows) {
+        if(err) {
+          throw new Error(err);
+          return;
+        }
+
         reply([{
           statusCode: 200,
           message: 'Inserted Successfully',
         }]);
-        if(err) {
-          throw new Error(err)
-        }
+        
       }
     );
     //end mysql
@@ -450,6 +625,26 @@ congController.postConfig = {
 
 };
 
+/* 
+===================================================
+- TESTING SALTING -
+=================================================== 
+*/
+//USER GET REQUEST
+userController_2.getConfig = {
+  handler: (req, res) => {
+    reply("hello");   
+  }
+  
+};
+
+
+//USER POST REQUEST
+userController_2.postConfig = {
+  handler: (req, res) => {
+    reply("hello");   
+  }
+};
 
 /* 
 ===================================================
@@ -469,7 +664,9 @@ var routes = [
   { path: '/event/{id?}', method: 'GET', config: eventController.getConfig },
   { path: '/event', method: 'POST', config: eventController.postConfig },
   { path: '/cong/{id?}', method: 'GET', config: congController.getConfig },
-  { path: '/cong', method: 'POST', config: congController.postConfig }
+  { path: '/cong', method: 'POST', config: congController.postConfig },
+  { path: '/api/user/{id?}', method: 'GET', config: userController_2.postConfig },
+  { path: '/api/user', method: 'POST', config: userController_2.postConfig }
 ];
 
 
