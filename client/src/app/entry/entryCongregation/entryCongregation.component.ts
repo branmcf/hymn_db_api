ed2505275989270896cbed3b26f6db34f9d23634
JustinSuh <jsuh@smu.edu>
2017-01-19 18:31:29 -0600

    moving responses to components file

--- client/src/app/entry/entryCongregation/entryCongregation.component.ts
+++ client/src/app/entry/entryCongregation/entryCongregation.component.ts
@@ -13,37 +13,41 @@ import { ContentfulService } from './../../services/contentful.service';
 
 export class EntryCongregationComponent {
   content: JSON;
-  name: string;
-  url: string;
-  city: string;
-  state: string;
-  country: any;
-  denom: any;
-  members: any;
-  type: any[];
-  instrument: any[];
-  shape: any;
-  attire: any;
-  location: any;
-  ethnicity: any[];
-  attendance: any;
+  // name: string;
+  // url: string;
+  // city: string;
+  // state: string;
+  // country: any;
+  // denom: any;
+  // members: any;
+  // type: any[];
+  // instrument: any[];
+  // shape: any;
+  // attire: any;
+  // location: any;
+  // ethnicity: any[];
+  // attendance: any;
 
-  submission: {
-    name: string;
-    url: string;
-    city: string;
-    state: string;
-    country: any;
-    denom: any;
-    members: any;
-    type: any[];
-    instrument: any[];
-    shape: any;
-    attire: any;
-    location: any;
-    ethnicity: any[];
-    attendance: any;
-  };
+  submission: any;
+
+  // submission: {
+  //   name: string;
+  //   url: string;
+  //   city: string;
+  //   state: string;
+  //   country: any;
+  //   denom: any;
+  //   members: any;
+  //   type: any[];
+  //   instrument: any[];
+  //   shape: any;
+  //   attire: any;
+  //   location: any;
+  //   ethnicity: any[];
+  //   attendance: any;
+  // };
+
+
 
   constructor(private route: ActivatedRoute,
     private router: Router,
@@ -55,23 +59,24 @@ export class EntryCongregationComponent {
   ngOnInit() {
     this.contentful.getCongregationForm().then((content) => {
       this.content = JSON.parse(content);
+      this.submission = {};
     });
 
     this.route.params.forEach(x => this.load(+x['user.id']));
-    this.name = '';
-    this.url = '';
-    this.city = '';
-    this.state = '';
-    this.country = '';
-    this.denom = '';
-    this.members = [];
-    this.type = [];
-    this.instrument = [];
-    this.shape = '';
-    this.attire = '';
-    this.location = '';
-    this.ethnicity = [];
-    this.attendance = '';
+    // this.name = '';
+    // this.url = '';
+    // this.city = '';
+    // this.state = '';
+    // this.country = '';
+    // this.denom = '';
+    // this.members = [];
+    // this.type = [];
+    // this.instrument = [];
+    // this.shape = '';
+    // this.attire = '';
+    // this.location = '';
+    // this.ethnicity = [];
+    // this.attendance = '';
 
     this.submission = {
       name: '',
@@ -91,6 +96,48 @@ export class EntryCongregationComponent {
     }
   }
 
+    typesOptions = [
+    "A hymn written prior to 1970",
+    "Newly composed hymn (within the last 10 years)",
+    "Praise and Worship Song (CCM)",
+    "Psalm Setting",
+    "Chant (Gregorian, Anglican, Pointed or Taize)",
+    "Older hymn text set to a new contemporary tune (or 're-tuned')",
+    "Song from another country (or 'World Song')",
+    "Secular Song"
+  ]
+  typesOptionsMap = {
+    "A hymn written prior to 1970" : false,
+    "Newly composed hymn (within the last 10 years)" : false,
+    "Praise and Worship Song (CCM)" : false,
+    "Psalm Setting" : false,
+    "Chant (Gregorian, Anglican, Pointed or Taize)" : false,
+    "Older hymn text set to a new contemporary tune (or 're-tuned')" : false,
+    "Song from another country (or 'World Song')" : false,
+    "Secular Song" : false
+  };
+  typesOptionsChecked = [];
+
+  initTypesOptionsMap() {
+      for (var x = 0; x < this.typesOptions.length; x++) {
+      this.typesOptionsMap[this.typesOptions[x]] = false;
+    }
+  }
+
+  updateCheckedOptions(option, event) {
+   this.typesOptionsMap[option] = event.target.checked;
+  }
+
+  updateOptions() {
+    for(var x in this.typesOptionsMap) {
+        if(this.typesOptionsMap[x]) {
+            this.typesOptionsChecked.push(x);
+        }
+    }
+    this.typesOptions = this.typesOptionsChecked;
+    this.typesOptionsChecked = [];
+  }
+
   private load(id) {
     if (!id) {
       return;
@@ -99,30 +146,15 @@ export class EntryCongregationComponent {
     var onload = (data) => {
       if (data) {
         this.submission = data;
-      } else {
+      }
+      else {
 
       }
     };
-
   }
 
-
-
   submit() {
-    // this.submission.title = this.title;
-    // this.submission.type = this.type;
-    // this.submission.url = this.url;
-    // this.submission.author = this.author;
-    // this.submission.parent = this.parent;
-    // this.submission.desc = this.desc;
-    // this.submission.category = this.category;
-    // this.submission.topic = this.topic;
-    // this.submission.accompany = this.accompany;
-    // this.submission.lang = this.lang;
-    // this.submission.ensemble = this.ensemble;
-    // this.submission.ethnicity = this.ethnicity;
-    // this.submission.isInvolved = this.isInvolved;
-    // this.submission.free = this.free
+    
   }
 
   next() {
