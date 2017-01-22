6afa81b5eb17ad5232624fddda2e5922bf16fbaf
TylerKirkpatrick <tkirkpatrick@smu.edu>
2017-01-22 14:56:16 -0600

    added cookie auth

--- server/server.js
+++ server/server.js
@@ -1,3 +1,113 @@
+var Hapi = require('hapi')
+var Good = require('good')
+var Vision = require('vision')
+var Users = require('./users-db')
+var Handlebars = require('handlebars')
+var CookieAuth = require('hapi-auth-cookie')
+
+// create new server instance
+var server = new Hapi.Server()
+
+// add server’s connection information
+server.connection({
+  host: 'localhost',
+  port: 3000
+})
+
+// register plugins to server instance
+server.register([
+  {
+    register: Vision
+  },
+  {
+    register: Good,
+    options: {
+      ops: {
+        interval: 10000
+      },
+      reporters: {
+        console: [
+          {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [ { log: '*', response: '*', request: '*' } ]
+          },
+          {
+            module: 'good-console'
+          },
+          'stdout'
+        ]
+      }
+    }
+  },
+  {
+    register: CookieAuth
+  }
+], function (err) {
+  if (err) {
+    server.log('error', 'failed to install plugins')
+
+    throw err
+  }
+
+  server.log('info', 'Plugins registered')
+
+  /**
+   * view configuration
+   */
+  server.views({
+    engines: {
+      html: Handlebars
+    },
+    path: __dirname + '/views',
+    layout: true
+  })
+  server.log('info', 'View configuration completed')
+
+  // validation function used for hapi-auth-cookie: optional and checks if the user is still existing
+  var validation = function (request, session, callback) {
+    var username = session.username
+    var user = Users[ username ]
+
+    if (!user) {
+      return callback(null, false)
+    }
+
+    server.log('info', 'user authenticated')
+    callback(err, true, user)
+  }
+
+  server.auth.strategy('session', 'cookie', true, {
+    password: 'm!*"2/),p4:xDs%KEgVr7;e#85Ah^WYC',
+    cookie: 'future-studio-hapi-tutorials-cookie-auth-example',
+    redirectTo: '/',
+    isSecure: false,
+    validateFunc: validation
+  })
+
+  server.log('info', 'Registered auth strategy: cookie auth')
+
+  var routes = require('./cookie-routes')
+  server.route(routes)
+  server.log('info', 'Routes registered')
+
+  // start your server after plugin registration
+  server.start(function (err) {
+    if (err) {
+      server.log('error', 'failed to start server')
+      server.log('error', err)
+
+      throw err
+    }
+
+    server.log('info', 'Server running at: ' + server.info.uri)
+  })
+})
+
+
+//EARLIEST VERSION:
+/*
+
 'use strict';
 var Joi = require('joi');
 const Hapi = require('hapi');
@@ -14,7 +124,7 @@ const Bcrypt = require('bcrypt');
 //authentication
 const BasicAuth = require('hapi-auth-basic');
 
-//const cookieAuth = require('hapi-auth-cookie');
+const CookieAuth = require('hapi-auth-cookie');
 
 
 //
@@ -26,11 +136,9 @@ server.connection({
     port: 8000 
 });
 
-/* 
-===================================================
-- BASIC AUTH -
-=================================================== 
-*/
+
+//- BASIC AUTH -
+
 
 var testUsers = {
   future: {
@@ -38,7 +146,8 @@ var testUsers = {
     username: 'future',
     password: '$2a$04$YPy8WdAtWswed8b9MfKixebJkVUhEZxQCrExQaxzhcdR2xMmpSJiG'  // 'studio'
   }
-}
+};
+
 
 var basicValidation = function (request, username, password, callback) {
     var user = testUsers[username]
@@ -47,33 +156,82 @@ var basicValidation = function (request, username, password, callback) {
 
     Bcrypt.compare(password, user.password, function(err, isValid) {
       callback(err, isValid, {id: user.id, username: user.username })
-    })
+    });
 
 
-}
+};
 
-server.register(BasicAuth, function (err) {
-    
-    server.auth.strategy('simple', 'basic', { validateFunc: basicValidation })
 
-    var routes = require('./routes/routes.js');
+server.registerCookieAuth, function(err) {
+    server.auth.strategy('session', 'cookie', options);
 
-    server.route(routes);
-
-    // for auth...
-    server.route({method: 'GET', path: '/private',
-        config: {
-            auth: 'simple',
+    // LOG OUT
+    server.route({
+      method: 'GET', 
+      path: '/private-route',
+      config: {
+            auth: 'session',
             handler: function(request, reply) {
-              reply('Yeah! This message only avaliable to auth users!')
+              //clear session data
+              request.cookieAuth.clear()
+              reply('Logged out!')
             }
         }
-    })
-    // end for auth...
+    });
+    // LOG OUT
 
+    //LOGIN POST
+    server.route({
+      method: 'POST',
+      path: '/login',
+      config: {
+        handler: function(request, reply) {
+          var username = request.payload.username
+          var password = request.payload.password
+
+          //check if user is in DB
+          //compare pass
+
+          //if everything went smooth
+          request.cookieAuth.set(user);
+
+          reply('Yeeeeeeehaw, great to see you again!')
+        }
+      }
+    })
+    //END LOGIN POST
+
+    //cookie test
+    server.route({
+      method: 'GET',
+      path: '/some-route',
+      config: {
+        auth: {
+          mode: 'try',
+          strategy: 'session'
+        },
+        handler: function(request, reply) {
+          if(request.auth.isAuthenticated) {
+            //session data available
+            var session = request.auth.credentials
+
+            return reply('Bro, you\'re already authenticated!');
+          }
+
+          //if not authenticated...
+        }
+      }
+
+    })
+    //end cookie test
 
     // Start the server
-    server.start((err) => {
+    
+
+
+};
+
+server.start((err) => {
 
         if (err) {
           throw err;
@@ -83,136 +241,4 @@ server.register(BasicAuth, function (err) {
         }
     });
 
-});
-
-
-
-
-
-/* 
-===================================================
-- END BASIC AUTH -
-=================================================== 
 */
-
-
-//
-//authentication
-//
-/*
-const Crypto = require('crypto');
-const onRequest = function (request, reply) {
-  const hash = Crypto.createHash('sha1');
-  request.on('peek', (chunk) => { hash.update(chunk); });
-  request.once('finish', () => { console.log(hash.digest('hex')); });
-  request.once('disconnect', () => { console.error('request aborted'); });
-  reply reply.continue();
-};
-
-server.ext('onRequest', onRequest);
-*/
-
-/*
-//
-// authentication with jwt test
-//
-server.register(require('hapi-auth-jwt'), (err) => {
-
-  // We're giving the strategy both a name
-  // and scheme of 'jwt'
-  server.auth.strategy('jwt', 'jwt', {
-    key: secret,
-    verifyOptions: { algorithms: ['HS256'] }
-  });
-
-  // Look through the routes in
-  // all the subdirectories of API
-  // and create a new route for each
-  glob.sync('api.js', {
-    root: __dirname
-  }).forEach(file => {
-    const route = require(path.join(__dirname, file));
-    server.route(route);
-  });
-});
-*/
-
-/* 
-===================================================
-- authentication with hapi-auth-basic test -
-=================================================== 
-*/
-
-/*
-const testUsers = {
-  john: {
-    id: 357,
-    email: 'John@gmail.com',
-    password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm', // 'secret'
-    first_name: 'john',
-    last_name: 'Doe'
-
-  }
-};
-
-const validate = function (request, first_name, password, callback) {
-  const testUser = testUsers[first_name];
-  if(!testUser) { 
-    console.log("error, user or password incorrect...");
-    return callback(null, false); 
-  }
-
-  bcrypt.compare(password, testUser.password, (err, isValid) => {
-    console.log(password, " then ", testUser.password);
-    callback(err, isValid, {id: testUser.id, first_name: testUser.first_name})
-  });
-};
-
-server.register(require('hapi-auth-basic'), (err)=> {
-  server.auth.strategy('simple', 'basic', { validateFunc: validate });
-  server.route({
-    method: 'GET',
-    path: '/auth',
-    config: {
-      auth: 'simple',
-      handler: function (req, reply) {
-        reply('hello, '+ req.auth.credentials.first_name);
-      }
-    }
-  });
-});
-*/
-
-/* 
-===================================================
-- authentication method 2 with hapi-auth-basic and cookie test -
-=================================================== 
-*/
-
-/*
-server.register(cookieAuth, function(err) {
-  server.auth.strategy('session', 'cookie', options)
-  // Start the server
-  server.start((err) => {
-
-    if (err) {
-      throw err;
-    }
-    else {
-      console.log('Server running at:', server.info.uri);
-    }
-  });
-});
-
-server.route({
-  method: 'GET',
-  path: '/auth',
-  config: {
-    auth: 'session',
-    handler: function (request, reply) {
-      reply('Authentication message');
-    }
-  }
-});
-*/
-
