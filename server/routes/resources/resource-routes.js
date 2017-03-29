d5a41f1efe95fab2566259a0ef0006c7dcc0495c
TylerKirkpatrick <tkirkpatrick@smu.edu>
2017-03-29 02:02:57 -0500

    unapproved isue fixed for resources

--- server/routes/resources/resource-routes.js
+++ server/routes/resources/resource-routes.js
@@ -303,66 +303,187 @@ resourceController.getConfig = {
 
   handler: function (request, reply) {
 
-    connection.query(`SELECT * from resources`, function(err, rows, fields) {
-    if (!err) {
+    if(!request.params.id) {
+      connection.query(`SELECT * from resources`, function(err, rows, fields) {
+      if (err) { return reply(Boom.badRequest()); }
 
-      var JSObj = rowsToJS(rows);
-      //var JSObj = rows;
+        var JSObj = rowsToJS(rows);
+        //var JSObj = rows;
 
-      resources = [];
-      //resTypes = [];
-      resCategories = [];
-      resTopics =[];
-      resAcc = [];
-      resLanguages = [];
-      resTags = [];
-      resEnsembles = [];
-      resEth = [];
-      resDenominations = [];
-      resInstruments = [];
+        resources = [];
+        //resTypes = [];
+        resCategories = [];
+        resTopics =[];
+        resAcc = [];
+        resLanguages = [];
+        resTags = [];
+        resEnsembles = [];
+        resEth = [];
+        resDenominations = [];
+        resInstruments = [];
 
-      resources = JSObj;
-      numRes = resources.length;
+        resources = JSObj;
+        numRes = resources.length;
 
-      for(var i=0; i<JSObj.length; i++) { 
-        popArray(JSObj[i]["ethnicities"], resEth);
-        popArray(JSObj[i]["categories"], resCategories);
-        popArray(JSObj[i]["topics"], resTopics);
-        popArray(JSObj[i]["accompaniment"], resAcc);
-        popArray(JSObj[i]["languages"], resLanguages);
-        popArray(JSObj[i]["ensembles"], resEnsembles);
-        popArray(JSObj[i]["tags"], resTags);
-        popArray(JSObj[i]["instruments"], resInstruments);
-        popArray(JSObj[i]["denominations"], resDenominations);
-        //popArray(JSObj[i]["types"], resTypes);
+        for(var i=0; i<JSObj.length; i++) { 
+          popArray(JSObj[i]["ethnicities"], resEth);
+          popArray(JSObj[i]["categories"], resCategories);
+          popArray(JSObj[i]["topics"], resTopics);
+          popArray(JSObj[i]["accompaniment"], resAcc);
+          popArray(JSObj[i]["languages"], resLanguages);
+          popArray(JSObj[i]["ensembles"], resEnsembles);
+          popArray(JSObj[i]["tags"], resTags);
+          popArray(JSObj[i]["instruments"], resInstruments);
+          popArray(JSObj[i]["denominations"], resDenominations);
+          //popArray(JSObj[i]["types"], resTypes);
 
-        resEth_all.push(resEth);
-        resCategories_all.push(resCategories);
-        resTopics_all.push(resTopics);
-        resAcc_all.push(resAcc);
-        resLanguages_all.push(resLanguages);
-        resEnsembles_all.push(resEnsembles);
-        resTags_all.push(resTags);
-        resInstruments_all.push(resInstruments);
-        resDenominations_all.push(resDenominations);
-        //popArray(JSObj[i]["types"], resTypes);
+          resEth_all.push(resEth);
+          resCategories_all.push(resCategories);
+          resTopics_all.push(resTopics);
+          resAcc_all.push(resAcc);
+          resLanguages_all.push(resLanguages);
+          resEnsembles_all.push(resEnsembles);
+          resTags_all.push(resTags);
+          resInstruments_all.push(resInstruments);
+          resDenominations_all.push(resDenominations);
+          //popArray(JSObj[i]["types"], resTypes);
 
-      }
-      
-      if (request.params.id) {
-          if ((numRes <= request.params.id - 1) || (0 > request.params.id - 1)) {
-            //return reply('Not enough resources in the database for your request').code(404);
-            return reply(Boom.notFound("Index out of range for Resources get request"));
+        }
+        
+        //if no ID specified
+        var objToReturn = [];
+
+        for(var i=0; i < resources.length; i++) {
+          //var bob = formatResource(i);   
+          if(resources[i].approved == 0) {
+            var str = {
+              id:     resources[i].id,
+              user:   resources[i].user,
+              title:  resources[i].name
+            }
+            objToReturn.push(str);
           }
-          //if (resources.length <= request.params.id - 1) return reply('Not enough resources in the database for your request').code(404);
-          var actualIndex = Number(request.params.id -1 );  
+          
+        }//end for
+
+        //console.log(objToReturn);
+        if(objToReturn.length <= 0) {
+          return reply(Boom.badRequest("nothing to return"));
+        } else {
+          reply(objToReturn);
+        }  
+      });
+    }
+      //
+      //
+      //
+
+      if (request.params.id) {
+          
+          //var actualIndex = Number(request.params.id -1 );  
 
           //create new object, convert to json
           //only get resources that are NOT approved
-          
-          if(resources[actualIndex].approved == false || resources[actualIndex].approved == 0) {
-            var str = formatResource(actualIndex);
-            return reply(str);
+          connection.query(`SELECT * from resources where id = ${request.params.id}`, function(err, rows, fields) {
+          if (err) { return reply(Boom.badRequest()); }
+
+            var JSObj = rowsToJS(rows[0]);
+            //var JSObj = rows;
+
+            resources = [];
+            //resTypes = [];
+            resCategories = [];
+            resTopics =[];
+            resAcc = [];
+            resLanguages = [];
+            resTags = [];
+            resEnsembles = [];
+            resEth = [];
+            resDenominations = [];
+            resInstruments = [];
+
+            resources = JSObj;
+            numRes = resources.length;
+
+            popArray(JSObj["ethnicities"], resEth);
+            popArray(JSObj["categories"], resCategories);
+            popArray(JSObj["ensembles"], resEnsembles);
+            popArray(JSObj["instruments"], resInstruments);
+            //popArray(JSObj[i]["types"], resTypes);
+
+          if(JSObj.approved == false || JSObj.approved == 0) {
+            
+            resourceData = {
+              id:             JSObj.id,
+              title:          JSObj.name,
+              type:           JSObj.type,
+              url:            JSObj.website,
+              author:         JSObj.author,
+              resource_date:  JSObj.resource_date,
+              description:    JSObj.description,
+              parent:         JSObj.parent,
+              is_active:      JSObj.is_active,
+              high_level:     JSObj.high_level,
+              city:           JSObj.city,
+              state:          JSObj.state,
+              country:        JSObj.country,
+              hymn_soc_member:JSObj.hymn_soc_member,
+              is_free:        JSObj.is_free,
+              user_id:        JSObj.user_id,
+              user:           JSObj.user,
+              pract_schol:    JSObj.pract_schol,
+              approved:       JSObj.approved,
+          /*
+              languages:      JSObj.languages,
+              ethnicities:    JSObj.ethnicities,
+              ensembles:      JSObj.ensembles,
+              categories:     JSObj.categories,
+              accompaniment: 	JSObj.accompaniment,
+              topics:         JSObj.topics,
+              tags:           JSObj.tags,
+              denominations:  JSObj.denominations,
+              instruments:    JSObj.instruments
+          */
+              languages:      resLanguages[0],
+              ethnicities:    resEth[0],
+              ensembles:      resEnsembles[0],
+              categories:     resCategories[0],
+              accompaniment: 	resAcc[0],
+              topics:         resTopics[0],
+              tags:           resTags[0],
+              denominations:  resDenominations[0],
+              instruments:    resInstruments[0]
+
+
+            };
+
+            resourceData.is_active = reformatTinyInt(resourceData.is_active, false);
+            resourceData.high_level = reformatTinyInt(resourceData.high_level, false);
+            resourceData.hymn_soc_member = reformatTinyInt(resourceData.hymn_soc_member, false);
+            resourceData.is_free = reformatTinyInt(resourceData.is_free, false);
+            resourceData.pract_schol = reformatTinyInt(resourceData.pract_schol, true);
+            resourceData.approved = reformatTinyInt(resourceData.approved, false);
+
+            //format 
+            /*
+            resourceData.ethnicities = JSON.parse(resourceData.ethnicities);
+            resourceData.tags = JSON.parse(resourceData.tags);
+            resourceData.topics = JSON.parse(resourceData.topics);
+            resourceData.languages = JSON.parse(resourceData.languages);
+            resourceData.ensembles = JSON.parse(resourceData.ensembles);
+            resourceData.accompaniment = JSON.parse(resourceData.accompaniment);
+            resourceData.categories = JSON.parse(resourceData.categories);
+            */
+            //end formatting
+
+            var theUrl = "/resource/" + String(JSObj.id);
+
+            var finalObj = {
+              url: theUrl,
+              data: resourceData
+            };
+
+            return reply(finalObj);
           } else {
             return reply(Boom.badRequest("The Resource you request is already approved"));
           }
@@ -370,39 +491,10 @@ resourceController.getConfig = {
 
 
         //return reply(resources[actualId]);
+
+      });
+
       }
-
-      //if no ID specified
-      var objToReturn = [];
-
-      for(var i=0; i < resources.length; i++) {
-        //var bob = formatResource(i);   
-        if(resources[i].approved == 0) {
-          var str = {
-            id:     resources[i].id,
-            user:   resources[i].user,
-            title:  resources[i].name
-          }
-          objToReturn.push(str);
-        }
-        
-      }//end for
-
-      //console.log(objToReturn);
-      if(objToReturn.length <= 0) {
-        return reply(Boom.badRequest("nothing to return"));
-      } else {
-        reply(objToReturn);
-      }  
-
-
-    }//end if no error...
-    else
-      console.log('Error while performing Resources Query.');
-
-    });
-
-    
 
   }//end handler
 };
