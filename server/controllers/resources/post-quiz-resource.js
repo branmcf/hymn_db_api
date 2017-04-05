7b06a01091262a31d4490815199d30e1ba40b949
TylerKirkpatrick <tkirkpatrick@smu.edu>
2017-04-05 11:28:59 -0500

    fixed resource formatting for resource-quiz

--- server/controllers/resources/post-quiz-resource.js
+++ server/controllers/resources/post-quiz-resource.js
@@ -27,13 +27,6 @@ if (process.env.JAWSDB_URL) {
 
 connection.connect();
 
-function rowsToJS(theArray) {
-    var temp = JSON.stringify(theArray);
-    temp = JSON.parse(temp);
-    //console.log(temp);
-    return temp;
-}
-
 function checkForTrue(obj, callback) {
     var tempArray = [];
     for (var key in obj) {
@@ -326,9 +319,30 @@ module.exports.postQuiz = {
                     [toReturn]
                 ], (err, rows, fields) => {
                     if (err) { return reply(Boom.badRequest("Error on last step: returning relevant resources")); }
-                    var jsobj = rowsToJS(rows);
-                    //console.log(jsobj);
-                    return reply(jsobj);
+                    //var jsobj = rowsToJS(rows);
+                    //return reply(jsobj);
+                    var resources = rowsToJS(rows);
+                    var toReturn = [];
+                    for (var i in resources) {
+                        var toPush = formatJSON(resources[i]);
+                        toPush["url"] = toPush["website"];
+                        toPush["title"] = toPush["name"];
+                        delete toPush["website"];
+                        delete toPush["name"];
+
+                        //toPush.is_active = reformatTinyInt(toPush.is_active);
+                        //toPush.high_level = reformatTinyInt(toPush.high_level);
+                        toPush.hymn_soc_member = reformatTinyInt(toPush.hymn_soc_member);
+                        toPush.is_free = reformatFree(toPush.is_free);
+                        toPush.pract_schol = reformatPractSchol(toPush.pract_schol);
+                        //toPush.approved = reformatTinyInt(toPush.approved);
+
+                        toReturn.push(toPush);
+                    }
+
+                    return reply(toReturn);
+
+
                 });
 
 
@@ -337,4 +351,97 @@ module.exports.postQuiz = {
 
 
         } //end handler
+}
+
+function reformatTinyInt(toFormat) {
+    switch (toFormat) {
+        case (1):
+            return ("true");
+            break;
+        case (0):
+            return ("false");
+            break;
+        case (2):
+            return ("partially");
+            break;
+    }
+}
+
+function reformatPractSchol(toFormat) {
+    if (toFormat == 1) {
+        return ("Practical");
+    } else if (toFormat == 0) {
+        return ("Scholarly");
+    } else if (toFormat == 2) {
+        return ("Both");
+    } else {
+        return (toFormat);
+    }
+}
+
+function reformatFree(toFormat) {
+    if (toFormat == 1) {
+        return ("Yes");
+    } else if (toFormat == 0) {
+        return ("No");
+    } else if (toFormat == 2) {
+        return ("We do not offer this");
+    } else {
+        return (toFormat);
+    }
+}
+
+function rowsToJS(theArray) {
+    var temp = JSON.stringify(theArray);
+    temp = JSON.parse(temp);
+    //console.log(temp);
+    return temp;
+}
+
+
+function formatJSON(resource) {
+    var json_columns = ["topics", "ensembles", "accompaniment", "languages", "categories", "ethnicities", "instruments", "tags", "clothing", "shape"];
+    for (var i in json_columns) {
+        if (resource[json_columns[i]]) { //if it exists...
+            resource[json_columns[i]] = JSON.parse(resource[json_columns[i]]);
+
+        } else {
+            //console.log("error, ", json_columns[i], " doesn't exist in resource");
+        }
+
+
+    }
+
+    //return the JSON columns in an array
+    var theKeys = [];
+
+    //loop through every column, check if it's true
+    for (var col_index in json_columns) {
+        if (resource[json_columns[col_index]]) { //if it exists...
+            var current_obj = resource[json_columns[col_index]];
+            for (var key in current_obj) { //if key is in the current object...
+                if (current_obj.hasOwnProperty(key)) {
+                    var TorForOther = current_obj[key]; //the corresponding value to the key:value pair which is either T,F or TorForOther
+                    if (key == 'Other' || key == 'other') {
+                        theKeys.push(current_obj[key]);
+                    } else if (TorForOther == 1) {
+                        key = key.replace(/_/g, " ");
+                        theKeys.push(key);
+                    } else { /* false, so don't add... */ }
+
+                }
+            }
+
+            resource[json_columns[col_index]] = theKeys;
+            theKeys = [];
+
+        } else {
+            //console.log("error, ", json_columns[i], " doesn't exist in resource");
+        }
+
+
+    } //done looping through certain column
+
+
+    return resource;
 }
