85293e940bcf8ba35f7c499c038bb65869f70fe3
TylerKirkpatrick <tkirkpatrick@smu.edu>
2017-08-25 19:50:15 -0500

    fixed returned formatting from searching

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -367,16 +367,32 @@ function getQueryAndReturnResults(whichTable, additionalArgs, request, callback)
             }
         }
 
-        console.log("resource_matches: ", resource_matches);
-        console.log("ids: ", arrayOfIDs);
-
         //2. Get name, author, type, website, description, parent (no JSON cols for now) from ALL APPROVED resources
         var query = connection.query(`SELECT * FROM ${whichTable} WHERE id in (?)`, [arrayOfIDs], (err, rows, fields) => {
             if (err) { return callback(true, null); }
             if (rows.length <= 0) { return callback("did not find any matching items, nothing to return", null); }
             var relevant_resources = rowsToJS(rows);
 
-            return callback(null, relevant_resources);
+            var toReturn = [];
+
+            for (var i in relevant_resources) {
+                var toPush = formatJSON(relevant_resources[i]);
+                toPush["url"] = toPush["website"];
+                toPush["title"] = toPush["name"];
+                delete toPush["website"];
+                delete toPush["name"];
+
+                //toPush.is_active = reformatTinyInt(toPush.is_active);
+                //toPush.high_level = reformatTinyInt(toPush.high_level);
+                toPush.hymn_soc_member = reformatTinyInt(toPush.hymn_soc_member);
+                toPush.is_free = reformatFree(toPush.is_free);
+                toPush.pract_schol = reformatPractSchol(toPush.pract_schol);
+                //toPush.approved = reformatTinyInt(toPush.approved);
+
+                toReturn.push(toPush);
+            }
+
+            return callback(null, toReturn);
         });
 
         //3. 
@@ -385,6 +401,110 @@ function getQueryAndReturnResults(whichTable, additionalArgs, request, callback)
 
 
     });
+}
+
+function formatJSON(resource) {
+    var json_columns = ["topics", "ensembles", "accompaniment", "languages", "categories", "ethnicities", "instruments", "clothing", "shape", "tags", "denominations"];
+    for (var i in json_columns) {
+        if (resource[json_columns[i]]) { //if it exists...
+            //check to see if it's an array...
+            if (Array.isArray(resource[json_columns[i]]) == true) {
+                //now do tags seperately and REMOVE DUPLICATES
+                var tagsWithoutDuplicates = require('../../controllers/shared/remove-duplicate-tags')(resource["tags"]);
+                resource["tags"] = tagsWithoutDuplicates;
+                //nothing...
+            } else if (json_columns[i] == "tags") {
+                try {
+                    var tagsWithoutDuplicates = JSON.parse(resource[json_columns[i]]);
+                    tagsWithoutDuplicates = require('./shared/remove-duplicate-tags')(tagsWithoutDuplicates);
+                    resource["tags"] = tagsWithoutDuplicates;
+                } catch (e) {
+                    console.log(e.message);
+                    resource[json_columns[i]] = JSON.parse(resource[json_columns[i]]);
+                    //console.log(resource[json_columns[i]]);
+                }
+
+            } else {
+                resource[json_columns[i]] = JSON.parse(resource[json_columns[i]]);
+            }
+        } else {
+            //console.log("error, ", json_columns[i], " doesn't exist in resource");
+        }
+    }
+
+    //return the JSON columns in an array
+    var theKeys = [];
+
+    //loop through every column, check if it's true
+    for (var col_index in json_columns) {
+        if (resource[json_columns[col_index]]) { //if it exists...
+            if (Array.isArray(resource[json_columns[col_index]])) { //if it's an array, just continue with the next iteration
+                continue;
+            }
+            var current_obj = resource[json_columns[col_index]];
+            for (var key in current_obj) { //if key is in the current object...
+                if (current_obj.hasOwnProperty(key)) {
+                    var TorForOther = current_obj[key]; //the corresponding value to the key:value pair which is either T,F or TorForOther
+                    if (key == 'Other' || key == 'other') {
+                        theKeys.push(current_obj[key]);
+                    } else if (TorForOther == 1) {
+                        key = key.replace(/_/g, " ");
+                        theKeys.push(key);
+                    } else { /* false, so don't add... */ }
+
+                }
+            }
+
+            resource[json_columns[col_index]] = theKeys;
+            theKeys = [];
+
+        } else {
+            //console.log("error, ", json_columns[i], " doesn't exist in resource");
+        }
+
+
+    } //done looping through certain column
+
+
+    return resource;
+}
+
+function reformatTinyInt(toFormat) {
+    switch (toFormat) {
+        case (1):
+            return ("true");
+            break;
+        case (0):
+            return ("false");
+            break;
+        case (2):
+            return ("partially");
+            break;
+    }
+}
+
+function reformatPractSchol(toFormat) {
+    if (toFormat == 1) {
+        return ("Practical");
+    } else if (toFormat == 0) {
+        return ("Scholarly");
+    } else if (toFormat == 2) {
+        return ("Both");
+    } else {
+        return (toFormat);
+    }
+}
+
+function reformatFree(toFormat) {
+    if (toFormat == 1) {
+        return ("Yes");
+    } else if (toFormat == 0) {
+        return ("No");
+    } else if (toFormat == 2) {
+        return ("We do not offer this");
+    } else {
+        return (toFormat);
+    }
 }
 
 function addToResIDObject(id, resource_matches) {
