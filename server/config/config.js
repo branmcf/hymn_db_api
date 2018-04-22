4c24ab8e291b1a3324d86d87cc9ad93fa2da636d
Brandon McFarland <branmcf@gmail.com>
2018-04-22 18:39:02 -0500

    removed /server/config/* from gitignore

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "host": "read.czmqpt4oq8dh.us-west-2.rds.amazonaws.com",
+    "user": "brandon",
+    "password": "NiceHymns",
+    "database": "ebdb",
+    "port": "3306"
+}
+
+// TEST
+
